d926ed520a2c08897cf6092e68fba27e1d0428c9
Gustavo Souza <gugu.ug@hotmail.com>
2023-07-28 16:34:50 -0300

    atualização

--- app.js
+++ app.js
@@ -59,9 +59,10 @@ app.engine(
 )
 
 //mongoose
+//db.mongoURI ||
 mongoose.Promise = global.Promise
 mongoose
-  .connect(db.mongoURI || process.env.DATABASE_URL, {
+  .connect(process.env.DATABASE_URL, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
